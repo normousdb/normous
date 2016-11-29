normousdb/normous

commit 27f895e8e80555c090f8b78eabbb52cc4bf3894a

SERVER-27052 changed RollbackChecker::reset() and checkForRollback() to pass through result from TaskExecutor::scheduleRemoteCommand
Former-commit-id: 06da357873b3500f39832dee914c06b1968d05ca

src/mongo/db/repl/data_replicator_test.cpp
@@ -985,7 +985,7 @@ TEST_F(InitialSyncTest, DataReplicatorPassesThroughRollbackCheckerScheduleError)
     setResponses(responses);
     playResponses();
     getExecutor().shutdown();
-    verifySync(getNet(), ErrorCodes::CallbackCanceled);
+    verifySync(getNet(), ErrorCodes::ShutdownInProgress);
 }
 
 TEST_F(InitialSyncTest, DataReplicatorPassesThroughOplogFetcherFailure) {

src/mongo/db/repl/rollback_checker.cpp
@@ -48,26 +48,25 @@ RollbackChecker::RollbackChecker(executor::TaskExecutor* executor, HostAndPort s
 
 RollbackChecker::~RollbackChecker() {}
 
-RollbackChecker::CallbackHandle RollbackChecker::checkForRollback(const CallbackFn& nextAction) {
-    return _scheduleGetRollbackId(
-        [this, nextAction](const RemoteCommandCallbackArgs& args) {
-            if (!args.response.isOK()) {
-                nextAction(args.response.status);
-                return;
-            }
-            if (auto rbidElement = args.response.data["rbid"]) {
-                int remoteRBID = rbidElement.numberInt();
+StatusWith<RollbackChecker::CallbackHandle> RollbackChecker::checkForRollback(
+    const CallbackFn& nextAction) {
+    return _scheduleGetRollbackId([this, nextAction](const RemoteCommandCallbackArgs& args) {
+        if (!args.response.isOK()) {
+            nextAction(args.response.status);
+            return;
+        }
+        if (auto rbidElement = args.response.data["rbid"]) {
+            int remoteRBID = rbidElement.numberInt();
 
-                UniqueLock lk(_mutex);
-                bool hadRollback = _checkForRollback_inlock(remoteRBID);
-                lk.unlock();
-                nextAction(hadRollback);
-            } else {
-                nextAction(Status(ErrorCodes::CommandFailed,
-                                  "replSetGetRBID command failed when checking for rollback"));
-            }
-        },
-        nextAction);
+            UniqueLock lk(_mutex);
+            bool hadRollback = _checkForRollback_inlock(remoteRBID);
+            lk.unlock();
+            nextAction(hadRollback);
+        } else {
+            nextAction(Status(ErrorCodes::CommandFailed,
+                              "replSetGetRBID command failed when checking for rollback"));
+        }
+    });
 }
 
 RollbackChecker::Result RollbackChecker::hasHadRollback() {
@@ -75,38 +74,35 @@ RollbackChecker::Result RollbackChecker::hasHadRollback() {
     Result result(true);
     auto cbh = checkForRollback([&result](const Result& cbResult) { result = cbResult; });
 
-    if (!cbh.isValid()) {
-        return Status(ErrorCodes::CallbackCanceled,
-                      "Rollback check failed due to callback cancelation");
+    if (!cbh.isOK()) {
+        return cbh.getStatus();
     }
 
-    _executor->wait(cbh);
+    _executor->wait(cbh.getValue());
     return result;
 }
 
-RollbackChecker::CallbackHandle RollbackChecker::reset(const CallbackFn& nextAction) {
-    return _scheduleGetRollbackId(
-        [this, nextAction](const RemoteCommandCallbackArgs& args) {
-            if (!args.response.isOK()) {
-                nextAction(args.response.status);
-                return;
-            }
-            if (auto rbidElement = args.response.data["rbid"]) {
-                int newRBID = rbidElement.numberInt();
+StatusWith<RollbackChecker::CallbackHandle> RollbackChecker::reset(const CallbackFn& nextAction) {
+    return _scheduleGetRollbackId([this, nextAction](const RemoteCommandCallbackArgs& args) {
+        if (!args.response.isOK()) {
+            nextAction(args.response.status);
+            return;
+        }
+        if (auto rbidElement = args.response.data["rbid"]) {
+            int newRBID = rbidElement.numberInt();
 
-                UniqueLock lk(_mutex);
-                _setRBID_inlock(newRBID);
-                lk.unlock();
+            UniqueLock lk(_mutex);
+            _setRBID_inlock(newRBID);
+            lk.unlock();
 
-                // Actual bool value does not matter because reset_sync()
-                // will convert non-error StatusWith<bool> to Status::OK.
-                nextAction(true);
-            } else {
-                nextAction(Status(ErrorCodes::CommandFailed,
-                                  "replSetGetRBID command failed when checking for rollback"));
-            }
-        },
-        nextAction);
+            // Actual bool value does not matter because reset_sync()
+            // will convert non-error StatusWith<bool> to Status::OK.
+            nextAction(true);
+        } else {
+            nextAction(Status(ErrorCodes::CommandFailed,
+                              "replSetGetRBID command failed when checking for rollback"));
+        }
+    });
 }
 
 Status RollbackChecker::reset_sync() {
@@ -114,12 +110,12 @@ Status RollbackChecker::reset_sync() {
     Status resetStatus = Status(ErrorCodes::CommandFailed, "RollbackChecker reset failed");
     auto cbh = reset([&resetStatus](const Result& result) { resetStatus = result.getStatus(); });
 
-    if (!cbh.isValid()) {
+    if (!cbh.isOK()) {
         return Status(ErrorCodes::CallbackCanceled,
                       "RollbackChecker reset failed due to callback cancelation");
     }
 
-    _executor->wait(cbh);
+    _executor->wait(cbh.getValue());
     return resetStatus;
 }
 
@@ -138,20 +134,11 @@ bool RollbackChecker::_checkForRollback_inlock(int remoteRBID) {
     return remoteRBID != _baseRBID;
 }
 
-RollbackChecker::CallbackHandle RollbackChecker::_scheduleGetRollbackId(
-    const RemoteCommandCallbackFn& nextAction, const CallbackFn& errorFn) {
+StatusWith<RollbackChecker::CallbackHandle> RollbackChecker::_scheduleGetRollbackId(
+    const RemoteCommandCallbackFn& nextAction) {
     executor::RemoteCommandRequest getRollbackIDReq(
         _syncSource, "admin", BSON("replSetGetRBID" << 1), nullptr);
-    auto cbh = _executor->scheduleRemoteCommand(getRollbackIDReq, nextAction);
-
-    if (cbh.getStatus() == ErrorCodes::ShutdownInProgress) {
-        return RollbackChecker::CallbackHandle();
-    }
-    if (!cbh.isOK()) {
-        errorFn(cbh.getStatus());
-        return RollbackChecker::CallbackHandle();
-    }
-    return cbh.getValue();
+    return _executor->scheduleRemoteCommand(getRollbackIDReq, nextAction);
 }
 
 void RollbackChecker::_setRBID_inlock(int rbid) {

src/mongo/db/repl/rollback_checker.h
@@ -46,10 +46,14 @@ class Mutex;
  *    sync source changes, make a new RollbackChecker.
  * 2) Call reset(), either synchronously or asynchronously, so that the RollbackChecker retrieves
  *    the state it needs from the sync source to check for rollbacks.
- * 3) Call checkForRollback(), passing in the next action to check if there was a rollback at the
- *    sync source. If there is a rollback or another error, the error function provided will be
- *    called. This error could be an UnrecoverableRollbackError, when there is a rollback.
- *    Alternatively, call hasHadRollback() which synchonously checks for a rollback and returns
+ * 3) Call checkForRollback(), passing in a 'nextAction' callback. This checks if there was a
+ *    rollback at the sync source. If there is a rollback or another error, the 'nextAction'
+ *    callback will be invoked. This error could be an UnrecoverableRollbackError, when there is a
+ *    rollback.
+ *    The only case where 'nextAction' is not called is when checkForRollback fails because the
+ *    task executor is unable to schedule the remote replSetGetRBID command. In this case,
+ *    checkForRollback() returns the error from executorTaskExecutor::scheduleRemoteCommand().
+ *    Alternatively, call hasHadRollback() which synchronously checks for a rollback and returns
  *    true if any error occurs. Checking for a rollback does not reset the baseline rbid.
  * 4) Repeat steps 2 and 3 as needed.
  *
@@ -73,7 +77,7 @@ public:
     // and then calls the nextAction with the rollback checker result. An error status
     // will be passed to the callback if the RBID cannot be determined or if
     // the callback was canceled.
-    CallbackHandle checkForRollback(const CallbackFn& nextAction);
+    StatusWith<CallbackHandle> checkForRollback(const CallbackFn& nextAction);
 
     // Synchronously checks if there has been a rollback and returns a boolean specifying if one
     // has occurred.
@@ -83,7 +87,7 @@ public:
     // status specifying what should occur next. The status will either be OK if there was no
     // error or another status if the command failed. The nextAction should account for
     // each of these cases.
-    CallbackHandle reset(const CallbackFn& nextAction);
+    StatusWith<CallbackHandle> reset(const CallbackFn& nextAction);
 
     // Synchronously calls reset and returns the Status of the command.
     Status reset_sync();
@@ -102,9 +106,9 @@ private:
     bool _checkForRollback_inlock(int remoteRBID);
 
     // Schedules a remote command to get the rbid at the sync source and then calls the nextAction.
-    // If there is an error scheduling the call, it calls the errorFn with the status.
-    CallbackHandle _scheduleGetRollbackId(const RemoteCommandCallbackFn& nextAction,
-                                          const CallbackFn& errorFn);
+    // If there is an error scheduling the call, it returns the error from
+    // TaskExecutor::scheduleRemoteCommand().
+    StatusWith<CallbackHandle> _scheduleGetRollbackId(const RemoteCommandCallbackFn& nextAction);
 
     // Assumes a lock has been taken. Sets the current rbid used as the baseline for rollbacks.
     void _setRBID_inlock(int rbid);

src/mongo/db/repl/rollback_checker_test.cpp
@@ -83,13 +83,13 @@ TEST_F(RollbackCheckerTest, InvalidConstruction) {
 TEST_F(RollbackCheckerTest, ShutdownBeforeStart) {
     auto callback = [](const RollbackChecker::Result&) {};
     getReplExecutor().shutdown();
-    ASSERT(!getRollbackChecker()->reset(callback));
-    ASSERT(!getRollbackChecker()->checkForRollback(callback));
+    ASSERT_NOT_OK(getRollbackChecker()->reset(callback).getStatus());
+    ASSERT_NOT_OK(getRollbackChecker()->checkForRollback(callback).getStatus());
 }
 
 TEST_F(RollbackCheckerTest, ShutdownBeforeHasHadRollback) {
     getReplExecutor().shutdown();
-    ASSERT_EQUALS(ErrorCodes::CallbackCanceled, getRollbackChecker()->hasHadRollback());
+    ASSERT_EQUALS(ErrorCodes::ShutdownInProgress, getRollbackChecker()->hasHadRollback());
 }
 
 TEST_F(RollbackCheckerTest, ShutdownBeforeResetSync) {
@@ -99,7 +99,7 @@ TEST_F(RollbackCheckerTest, ShutdownBeforeResetSync) {
 
 TEST_F(RollbackCheckerTest, reset) {
     auto callback = [](const RollbackChecker::Result&) {};
-    auto cbh = getRollbackChecker()->reset(callback);
+    auto cbh = unittest::assertGet(getRollbackChecker()->reset(callback));
     ASSERT(cbh);
 
     auto commandResponse = BSON("ok" << 1 << "rbid" << 3);
@@ -118,7 +118,7 @@ TEST_F(RollbackCheckerTest, RollbackRBID) {
         _hasCalledCallback = true;
     };
     // First set the RBID to 3.
-    auto refreshCBH = getRollbackChecker()->reset(callback);
+    auto refreshCBH = unittest::assertGet(getRollbackChecker()->reset(callback));
     ASSERT(refreshCBH);
     auto commandResponse = BSON("ok" << 1 << "rbid" << 3);
     getNet()->scheduleSuccessfulResponse(commandResponse);
@@ -133,7 +133,7 @@ TEST_F(RollbackCheckerTest, RollbackRBID) {
     }
 
     // Check for rollback
-    auto rbCBH = getRollbackChecker()->checkForRollback(callback);
+    auto rbCBH = unittest::assertGet(getRollbackChecker()->checkForRollback(callback));
     ASSERT(rbCBH);
 
     commandResponse = BSON("ok" << 1 << "rbid" << 4);
@@ -156,7 +156,7 @@ TEST_F(RollbackCheckerTest, NoRollbackRBID) {
         _hasCalledCallback = true;
     };
     // First set the RBID to 3.
-    auto refreshCBH = getRollbackChecker()->reset(callback);
+    auto refreshCBH = unittest::assertGet(getRollbackChecker()->reset(callback));
     ASSERT(refreshCBH);
     auto commandResponse = BSON("ok" << 1 << "rbid" << 3);
     getNet()->scheduleSuccessfulResponse(commandResponse);
@@ -171,7 +171,7 @@ TEST_F(RollbackCheckerTest, NoRollbackRBID) {
     }
 
     // Check for rollback
-    auto rbCBH = getRollbackChecker()->checkForRollback(callback);
+    auto rbCBH = unittest::assertGet(getRollbackChecker()->checkForRollback(callback));
     ASSERT(rbCBH);
 
     commandResponse = BSON("ok" << 1 << "rbid" << 3);

commit 3bef465054bde87d84aeb564af1a74ce51aa06e9

SERVER-27052 fixed DataReplicatorPassesThroughRollbackCheckerScheduleError test to reject only the replSetGetRBID schedule request
This commit addresses an issue introduced in 27f895e8e80555c090f8b78eabbb52cc4bf3894a [formerly 06da357873b3500f39832dee914c06b1968d05ca].


Former-commit-id: a73e65ba511d047431b85138b010818800b7de04

src/mongo/db/repl/SConscript
@@ -1069,6 +1069,7 @@ env.CppUnitTest(
         '$BUILD_DIR/mongo/db/query/command_request_response',
         '$BUILD_DIR/mongo/executor/thread_pool_task_executor_test_fixture',
         '$BUILD_DIR/mongo/unittest/concurrency',
+        '$BUILD_DIR/mongo/unittest/task_executor_proxy',
     ],
 )
 

src/mongo/db/repl/data_replicator_test.cpp
@@ -50,6 +50,7 @@
 #include "mongo/executor/network_interface_mock.h"
 #include "mongo/executor/thread_pool_task_executor_test_fixture.h"
 #include "mongo/stdx/mutex.h"
+#include "mongo/unittest/task_executor_proxy.h"
 #include "mongo/util/concurrency/old_thread_pool.h"
 #include "mongo/util/concurrency/thread_name.h"
 #include "mongo/util/fail_point_service.h"
@@ -78,6 +79,35 @@ struct CollectionCloneInfo {
     CollectionMockStats stats;
     CollectionBulkLoaderMock* loader = nullptr;
     Status status{ErrorCodes::NotYetInitialized, ""};
+};
+
+class TaskExecutorMock : public unittest::TaskExecutorProxy {
+public:
+    using ShouldFailRequestFn = stdx::function<bool(const executor::RemoteCommandRequest&)>;
+
+    TaskExecutorMock(executor::TaskExecutor* executor, ShouldFailRequestFn shouldFailRequest)
+        : unittest::TaskExecutorProxy(executor), _shouldFailRequest(shouldFailRequest) {}
+
+    StatusWith<CallbackHandle> scheduleWorkAt(Date_t when, const CallbackFn& work) {
+        if (shouldFailScheduleWorkAt) {
+            return Status(ErrorCodes::OperationFailed,
+                          str::stream() << "failed to schedule work at " << when.toString());
+        }
+        return getExecutor()->scheduleWorkAt(when, work);
+    }
+
+    StatusWith<CallbackHandle> scheduleRemoteCommand(const executor::RemoteCommandRequest& request,
+                                                     const RemoteCommandCallbackFn& cb) override {
+        if (_shouldFailRequest(request)) {
+            return Status(ErrorCodes::OperationFailed, "failed to schedule remote command");
+        }
+        return getExecutor()->scheduleRemoteCommand(request, cb);
+    }
+
+    bool shouldFailScheduleWorkAt = false;
+
+private:
+    ShouldFailRequestFn _shouldFailRequest;
 };
 
 class DataReplicatorTest : public executor::ThreadPoolExecutorTest, public SyncSourceSelector {
@@ -246,6 +276,12 @@ protected:
 
         launchExecutorThread();
 
+        _shouldFailRequest = [](const executor::RemoteCommandRequest&) { return false; };
+        _executorProxy = stdx::make_unique<TaskExecutorMock>(
+            &getExecutor(), [this](const executor::RemoteCommandRequest& request) {
+                return _shouldFailRequest(request);
+            });
+
         _myLastOpTime = OpTime({3, 0}, 1);
 
         DataReplicatorOptions options;
@@ -264,7 +300,7 @@ protected:
         };
 
         auto dataReplicatorExternalState = stdx::make_unique<DataReplicatorExternalStateMock>();
-        dataReplicatorExternalState->taskExecutor = &getExecutor();
+        dataReplicatorExternalState->taskExecutor = _executorProxy.get();
         dataReplicatorExternalState->dbWorkThreadPool = &getDbWorkThreadPool();
         dataReplicatorExternalState->currentTerm = 1LL;
         dataReplicatorExternalState->lastCommittedOpTime = _myLastOpTime;
@@ -336,6 +372,8 @@ protected:
         }
     }
 
+    TaskExecutorMock::ShouldFailRequestFn _shouldFailRequest;
+    std::unique_ptr<TaskExecutorMock> _executorProxy;
 
     DataReplicatorOptions::SetMyLastOptimeFn _setMyLastOptime;
     OpTime _myLastOpTime;
@@ -928,6 +966,21 @@ TEST_F(InitialSyncTest, FailOnRollback) {
 }
 
 TEST_F(InitialSyncTest, DataReplicatorPassesThroughRollbackCheckerScheduleError) {
+    // Make the second replSetGetRBID command fail. Allow all other requests to be scheduled.
+    executor::RemoteCommandRequest request;
+    bool first = true;
+    _shouldFailRequest = [&first, &request](const executor::RemoteCommandRequest& requestToSend) {
+        if ("replSetGetRBID" == requestToSend.cmdObj.firstElement().fieldNameStringData()) {
+            if (first) {
+                first = false;
+                return false;
+            }
+            request = requestToSend;
+            return true;
+        }
+        return false;
+    };
+
     const Responses responses =
         {
             // get rollback id
@@ -977,7 +1030,7 @@ TEST_F(InitialSyncTest, DataReplicatorPassesThroughRollbackCheckerScheduleError)
                       << OplogEntry::kOplogVersion
                       << ", op:'i', o:{_id:1, c:1}}]}}")},
             // Response to replSetGetRBID request is left out so that we can cancel the request by
-            // shutting the executor down.
+            // rejecting the executor::TaskExecutor::scheduleRemoteCommand() request.
         };
 
     startSync(1);
@@ -985,7 +1038,7 @@ TEST_F(InitialSyncTest, DataReplicatorPassesThroughRollbackCheckerScheduleError)
     setResponses(responses);
     playResponses();
     getExecutor().shutdown();
-    verifySync(getNet(), ErrorCodes::ShutdownInProgress);
+    verifySync(getNet(), ErrorCodes::OperationFailed);
 }
 
 TEST_F(InitialSyncTest, DataReplicatorPassesThroughOplogFetcherFailure) {